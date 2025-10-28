c58cd898f37cc83e1c4d4e9e1ada8c8be3be662e
ferrik <50550603+ferrik@users.noreply.github.com>
2025-10-28 09:18:42 +0200

    Update __init__.py

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,1 +1,30 @@
+"""
+📋 Обробники команд Telegram бота
+"""
+from .commands import (
+    start_handler,
+    menu_handler,
+    cart_handler,
+    order_handler,
+    help_handler,
+    cancel_handler,
+    choose_partner_handler,
+    promocode_handler,
+    history_handler
+)
+from .messages import message_handler
+from .callbacks import callback_query_handler
 
+__all__ = [
+    'start_handler',
+    'menu_handler',
+    'cart_handler',
+    'order_handler',
+    'help_handler',
+    'cancel_handler',
+    'choose_partner_handler',
+    'promocode_handler',
+    'history_handler',
+    'message_handler',
+    'callback_query_handler'
+]

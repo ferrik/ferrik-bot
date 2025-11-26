1d60cfc0da9b5463781b8b143bc1509f7276aa65
ferrik <50550603+ferrik@users.noreply.github.com>
2025-11-26 13:17:07 +0200

    Update client.js

--- webapp/src/api/client.js
+++ webapp/src/api/client.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
 
+const API_BASE = import.meta.env.VITE_API_URL || 'https://ferrik-bot-zvev.onrender.com/api/v1';
+
+export const api = {
+  getMenu: (params) => axios.get(`${API_BASE}/menu`, { params }),
+  getMoodMenu: (tag) => axios.get(`${API_BASE}/menu/mood/${tag}`),
+  getRestaurants: () => axios.get(`${API_BASE}/restaurants`),
+  createOrder: (data) => axios.post(`${API_BASE}/order`, data),
+  validatePromo: (code) => axios.post(`${API_BASE}/promo/validate`, { code }),
+  getUserOrders: (userId) => axios.get(`${API_BASE}/orders/user/${userId}`)
+};
